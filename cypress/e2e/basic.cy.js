9d383e7778d7fdea195710d9adaa80a685f32a86
mec-lgap-demo <131414990+mec-lgap-demo@users.noreply.github.com>
2023-04-21 07:59:49 +0000

    mod test for del  products and values

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,17 +2,5 @@ describe('empty spec', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  it('navigates to the product page', () => {
-    cy.get('a[href="/products"]').eq(0).click();
-    cy.url().should("include", "/products")
-    cy.get('h1').contains(/Our Coffee/i)
-  })
-
-  it('navigates to the values page', () => {
-    cy.get('a[href="/values"]').eq(0).click();
-    cy.url().should("include", "/values")
-    cy.get('h1').contains(/Values/i)
-  })
 })
 
